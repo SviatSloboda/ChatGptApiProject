92249d20875dcc8c1f54e1dd077d6607bf45765b
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-20 09:22:30 +0200

    Setup project (spring initializr mit lombok, web, reactive web)

---
+++ src/main/java/com/example/chatgptbasedcookingingredients/ChatgptBasedCookingIngredientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptbasedcookingingredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptBasedCookingIngredientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptBasedCookingIngredientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatgptbasedcookingingredients/ChatgptBasedCookingIngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptbasedcookingingredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptBasedCookingIngredientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
